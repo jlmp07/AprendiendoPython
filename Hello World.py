17c2f57888dd2b90f72b64a45e1bf393ecdef630
jlmp07 <juanluismejia@gmail.com>
2020-07-22 23:11:00 -0400

    initial Commit

---
+++ Hello World.py
@@ -0,0 +1,36 @@
+# Clear the terminal screen
+import os
+os.system('cls')
+
+#Variables / Data Types: String, Numbers, Lists, Tuples, Dictionaries, Boolean
+
+#strings with " ", or ''
+first_name = 'John'
+last_name = 'Mejia'
+
+#numbers
+age = 41
+
+#list
+names = ["john", "Bob", "Patricio"]
+
+#Tuples
+names_2 = ("john", "Bob", "Patricio")
+
+#Dictionaries
+
+names_3 = {
+    "John" : "Pepperoni",
+    "Bob" : "Mushroon",
+    "Mary" : "Cheese"
+}
+
+
+#Boolean return True or False
+
+
+
+# Print the results
+print (names_3["John"])
+
+
